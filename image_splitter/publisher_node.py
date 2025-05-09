d3cea816e3ef5f69e007e10ec4d549eca5c32432
Hilkka1 <hilkkanelly1@gmail.com>
2025-05-09 19:49:07 +0300

    image_splitter/publisher_node.py:
    	- split_image-function
    		- added the size of the projected image as variables.
    		- resized the splitted image before publishing it.
    
    	- (new function) split_video
    		- started working on new function to maybe later handle videos with cv2
    
    	-publish_image_pieces
    		- modified the node to take a path to a directory
    		- the code now reads the images from the directory and refreshes the published image currently every 30 seconds

--- image_splitter/publisher_node.py
+++ image_splitter/publisher_node.py
@@ -4,10 +4,14 @@ import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from custom_messages.msg import ImagePiece
+import sys
+import os
+import time
 
 class ImageSplitterPublisher(Node):
-    def __init__(self):
+    def __init__(self, image_directory_path):
         super().__init__('image_splitter_publisher')
+        self.image_directory_path = image_directory_path
 
         """
         # This is most likely not the best way.
@@ -26,6 +30,11 @@ class ImageSplitterPublisher(Node):
     # num_rows and num_cols need to be modified to change the number of image pieces
     def split_image(self, image, num_rows=4, num_cols=2):
         pieces = []
+        proj_image_width = 1680
+        proj_image_height = 1050
+        # resize the image to fit the projector aspect ratio
+        #image = cv2.resize(image, (proj_image_width, proj_image_height))
+        
         height , width, channels = image.shape
         piece_height = height // num_rows
         piece_width = width // num_cols
@@ -33,40 +42,100 @@ class ImageSplitterPublisher(Node):
             for j in range(num_cols):
                 piece = image[i * piece_height:(i+1) * piece_height,
                         j * piece_width:(j + 1) * piece_width]
+                piece = cv2.resize(piece,(proj_image_width, proj_image_height))
                 pieces.append(piece)
                 
         return pieces
-
-    def publish_image_pieces(self, image_path):
     
-        image = cv2.imread(image_path)
-                   
-        pieces = self.split_image(image)
-        self.get_logger().info(f"Image split into {len(pieces)} pieces!")
+    def split_video(self, video_path, num_rows=4, num_cols=2):
+    
+        video_pieces = []
+        indice_of_piece = 0
         
-        # Saving image to file for debug purposes 
-        for i in range(0,len(pieces)):
-            cv2.imwrite(f"/home/rlab/imagePart{i}.png", pieces[i])
+        video = cv2.VideoCapture(video_path)
+        # getting the video stats
+        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = int(video.get(cv2.CAP_PROP_FPS))
+        
+        video_piece_height = height//num_rows
+        video_piece_width = width//num_cols
+        
+     
+        # setting up the output video
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+        
+        for i in range(num_rows):
+            for j in range(num_cols):
+                while True:
+            
+                    output_video = cv2.VideoWriter(f"output_video{indice_of_piece}.mp4",fourcc, fps, (video_piece_width,video_piece_height))
+                
+                    ret, frame = video.read()
+                    
+                    if not ret:
+                        break
+                
+                    cropped_frame = frame[i * video_piece_height:(i+1) * video_piece_height,
+                            j * video_piece_width:(j + 1) * video_piece_width]
+                    
+                    output_video.write(cropped_frame)
+                output_video.release()
+                indice_of_piece += 1
+                
 
-        for idx, piece in enumerate(pieces):
-            if idx+1 < len(pieces)+1:
-                msg = ImagePiece()
-                msg.piece_id = idx+1
-                msg.image = self.bridge.cv2_to_imgmsg(piece, encoding="bgr8")
-                self.publisher.publish(msg)
+    
+
+    def publish_image_pieces(self, video_path):
+        print(self.image_directory_path)
+        image_paths = sorted(os.listdir(self.image_directory_path))
+        print(image_paths)
+        for image_path in image_paths:
+                absolute_path = self.image_directory_path + "/" + image_path
+                print(image_path)
+                image = cv2.imread(absolute_path)
                 
-                self.get_logger().info(f"Published image piece {idx}.")
+                pieces = self.split_image(image)
+
+                #self.get_logger().info(f"Image split into {len(pieces)} pieces!")
                 
-            else:
-                self.get_logger().warn(f"Not enough pieces to publish for index  {idx}")
+                # Saving image to file for debug purposes 
+                for i in range(0,len(pieces)):
+                    cv2.imwrite(f"/home/rlab/imagePart{i}.png", pieces[i])
+
+                for idx, piece in enumerate(pieces):
+                    if idx+1 < len(pieces)+1:
+                        msg = ImagePiece()
+                        msg.piece_id = idx+1
+                        msg.image = self.bridge.cv2_to_imgmsg(piece, encoding="bgr8")
+                        self.publisher.publish(msg)
+                        
+                        #self.get_logger().info(f"Published image piece {idx}.")
+                        
+                    else:
+                        self.get_logger().warn(f"Not enough pieces to publish for index  {idx}")
+                # make the system wait until the next image is published.
+                time.sleep(30)
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = ImageSplitterPublisher()
+    
+    if len(sys.argv) < 2:
+        print("Usage: image_piece_subscriber.py <piece_id>")
+        sys.exit(1)
 
-    image_path = '/home/rlab/img.png'
-    node.publish_image_pieces(image_path)
+    try:
+        image_directory_path = str(sys.argv[1])
+    except ValueError:
+        print(":(.")
+        sys.exit(1)
+        
+    node = ImageSplitterPublisher(image_directory_path)
+
+
+    video_path = '/home/rlab/testVideos/videoOfSky.mp4'
+    node.publish_image_pieces(video_path)
     
     
     try:
