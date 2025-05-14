b68ab35bca3d6b47cb44ec7825ac9c5312a37725
Hilkka1 <hilkkanelly1@gmail.com>
2025-05-14 18:04:20 +0300

    image_splitter/publisher_node:
        - split_image():
            - the function now adds the red and green grids on the image to be publihsed
    
        - (new function) add_goal():
            - the function asks the user for the coordinates for the goal circle
            - the function returns an image with the goal circle added
    
        - publish_image():
            - the function now asks the user the time interval to change the images as seconds

--- image_splitter/publisher_node.py
+++ image_splitter/publisher_node.py
@@ -34,8 +34,29 @@ class ImageSplitterPublisher(Node):
         proj_image_height = 1050
         # resize the image to fit the projector aspect ratio
         #image = cv2.resize(image, (proj_image_width, proj_image_height))
-        
         height , width, channels = image.shape
+        
+        # add the coordinate lines to the image
+        red = (0,0,255)
+        green = (0,255,0)
+        
+        # vertical red line
+        image = cv2.line(image, (width//2, 0), (width//2, height), red,3)
+        # horizontal red line
+        image = cv2.line(image, (0,height//2), (width , height//2), red,3)
+        
+        # vertical green lines
+        image = cv2.line(image, (width//4, 0), (width//4, height), green,2)
+        image = cv2.line(image, (width//4*3, 0), (width//4*3, height), green,2)
+        # horizontal green lines
+        image = cv2.line(image, (0,height//4), (width , height//4), green,2)
+        image = cv2.line(image, (0,height//4*3), (width , height//4*3), green,3)
+        
+        
+        cv2.imwrite(f"/home/rlab/imagered.png", image)
+        
+        
+        
         piece_height = height // num_rows
         piece_width = width // num_cols
         for i in range(num_rows):
@@ -83,25 +104,40 @@ class ImageSplitterPublisher(Node):
                 output_video.release()
                 indice_of_piece += 1
                 
-
+    def add_goal(self, image):
+        # ask user for the coordintes for the goal
+        
+        x = int(input("Please add the x-coordinate of the goal as an integer."))
+        y = int(input("Please add the y-coordinate of the goal as an integer."))
+        
+        goal_diameter = 10
+        goal_width = 3
+        return cv2.circle(image, (x,y),goal_diameter,(0,0,255),goal_width)
     
 
     def publish_image_pieces(self, video_path):
         print(self.image_directory_path)
         image_paths = sorted(os.listdir(self.image_directory_path))
         print(image_paths)
+        
+        
+        # Ask user for the time interval as seconds
+        time_interval = int(input("Please input the time interval of the image change in seconds: "))
+        
         for image_path in image_paths:
                 absolute_path = self.image_directory_path + "/" + image_path
                 print(image_path)
                 image = cv2.imread(absolute_path)
                 
-                pieces = self.split_image(image)
+                goal_added = self.add_goal(image)
+                pieces = self.split_image(goal_added)
+                
 
                 #self.get_logger().info(f"Image split into {len(pieces)} pieces!")
                 
                 # Saving image to file for debug purposes 
-                for i in range(0,len(pieces)):
-                    cv2.imwrite(f"/home/rlab/imagePart{i}.png", pieces[i])
+                #for i in range(0,len(pieces)):
+                #    cv2.imwrite(f"/home/rlab/imagePart{i}.png", pieces[i])
 
                 for idx, piece in enumerate(pieces):
                     if idx+1 < len(pieces)+1:
@@ -115,7 +151,7 @@ class ImageSplitterPublisher(Node):
                     else:
                         self.get_logger().warn(f"Not enough pieces to publish for index  {idx}")
                 # make the system wait until the next image is published.
-                time.sleep(30)
+                time.sleep(time_interval)
 
 
 def main(args=None):
